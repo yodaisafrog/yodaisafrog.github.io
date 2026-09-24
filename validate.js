2ae7b74022be6fee68259fc3ada617c8fff56bb5
Ryan Anderson <ryanfanderson517@gmail.com>
2026-09-24 11:28:37 -0400

    validate

---
+++ validate.js
@@ -0,0 +1,6 @@
+(function() {
+  var script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/gh/gracehoppercenter/validate@1.0.5/validate.js";
+  script.async = false;
+  document.head.appendChild(script);
+})();
